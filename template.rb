2263dc935c64879d0e09ea1691ed23df34cb77a6
dinatih <dinatih@gmail.com>
2017-04-07 23:56:39 +0200

    Create template.rb

---
+++ template.rb
@@ -0,0 +1,7 @@
+# @dinatih Rails Application Template
+# Postgres, Rspec, Capybara, Haml, Bower, Twitter Bootsrap, Heroku, Github, Codeclimate (rubocop...), Travis/CircleCI, ...
+#
+# http://guides.rubyonrails.org/rails_application_templates.html
+# Usage:
+# $ rails new business_information_system -m https://github.com/dinatih/dinatih/template.rb
+# $ rails new business_information_system -T -d postgresql -m https://github.com/dinatih/dinatih/template.rb
