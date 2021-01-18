8ea0b2a6925ec370bca561480be1c079f481d566
dinatih <dinatih@gmail.com>
2021-01-18 23:27:08 +0100

    Add rails_template.rb

---
+++ workstation/rails_template.rb
@@ -0,0 +1,12 @@
+# @dinatih Rails Application Template
+# PostgreSQL, Rspec, FactoryBot, Capybara, Haml, Twitter Bootsrap, Heroku, Github, Rubocop)
+#
+# http://guides.rubyonrails.org/rails_application_templates.html
+# Usage:
+# $ rails new business_information_system -m https://github.com/dinatih/dinatih/template.rb
+# $ rails new business_information_system -T -d postgresql -m https://github.com/dinatih/dinatih/template.rb
+
+gem "postgres"
+gem "rspec"
+gem "factory_bot"
+gem "rubocop"
