ee089b1ad2a3b0c0b24733b5d2083738804f54f2
dinatih <dinatih@gmail.com>
2021-01-24 22:27:20 +0100

    refactor: use templates file

---
+++ workstation/rails_template/app/controllers/users_controller.rb
@@ -0,0 +1,69 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %w[ show edit update destroy ]
+
+  def index
+    if params[:organization_id]
+      @organization = Organization.find(params[:organization_id])
+      @users = @organization.users.all
+      render layout: 'organizations'
+    end
+  end
+
+  def show
+    if params[:organization_id]
+      @organization = Organization.find(params[:organization_id])
+      @user = @organization.users.find(params[:user_id])
+      render layout: 'organizations'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :organization_id)
+    end
+end
---
+++ workstation/rails_template/db/seeds.rb
@@ -0,0 +1,5 @@
+# [OPTIMIZE] Create a module or class to be able to include this:
+# include FactoryBot::Syntax::Methods
+4.times do
+  FactoryBot.create :organization
+end
