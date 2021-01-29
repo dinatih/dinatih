eeb90df6be08c04c2cdaff7aa1c54f1ed80f553b
dinatih <dinatih@gmail.com>
2021-01-29 12:42:21 +0100

    feat: add templates

--- workstation/rails_template/db/seeds.rb
+++ workstation/rails_template/db/seeds.rb
@@ -1,5 +1,6 @@
 # [OPTIMIZE] Create a module or class to be able to include this:
 # include FactoryBot::Syntax::Methods
-4.times do
+6.times do
   FactoryBot.create :organization
+  Faker::UniqueGenerator.clear
 end
